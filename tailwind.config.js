858607948299df936c28b5305f2d365441ece500
Murillo Camargo <murillo@murillocamargo.com.br>
2024-01-30 12:39:16 -0800

    update files and remove project related code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,151 +3,81 @@
 const plugin = require('tailwindcss/plugin');
 
 function toRem(px) {
-  return `${px / 16}rem`;
+	return `${px / 16}rem`;
 }
 
 module.exports = {
-  content: ["./**/*.php"],
-  darkMode: 'class',
-  theme: {
-    container: {
-      center: true,
-      padding: "1.5rem",
-      screens: {
-        sm: "768px",
-        md: "1024px",
-        lg: "1280px",
-        xl: "1536px",
-      },
-    },
-    fontSize: {
-      10: toRem(10),
-      11: toRem(11),
-      12: toRem(12),
-      13: toRem(13),
-      14: toRem(14),
-      15: toRem(15),
-      16: toRem(16),
-      17: toRem(17),
-      18: toRem(18),
-      20: toRem(20),
-      22: toRem(22),
-      24: toRem(24),
-      26: toRem(26),
-      28: toRem(28),
-      30: toRem(30),
-      32: toRem(32),
-      35: toRem(35),
-      40: toRem(40),
-      45: toRem(45),
-      50: toRem(50),
-      56: toRem(56),
-      60: toRem(60),
-      70: toRem(70),
-      80: toRem(80),
-      90: toRem(90),
-      120: toRem(120),
-    },
-    extend: {
-      // Font being loaded on /source/scripts/utilities/webfonts.ts
-      fontFamily: {
-        "bvg-primary": ["Montserrat", "sans-serif"],
-        "bvg-sans": ["Roboto", "sans-serif"],
-        "bmo-primary": ["din-condensed", "sans-serif"],
-        "bmo-sans": ["proxima-nova", "sans-serif"],
-      },
-      colors: {
-        "primary-bmo": "#0F1138",
-        "orange-bmo": "#EB6600",
-        "yellow-bmo": "#FFCA7D",
-        "blue-bmo": "#99B6F2",
-        "gray-bmo-light": "#D9D9D9",
-        "gray-bmo-dark": "#353535",
-        "primary-bvg": "#00263D",
-        "primary-bvg-100": "#6995BF",
-        "primary-bvg-200": "#3869A9",
-        "primary-bvg-300": "#21275D",
-        "yellow-bvg-100": "#E2BA47",
-        "yellow-bvg-200": "#D49635",
-        "ring-start-bvg": "#D49635",
-        "ring-end-bvg": "#6995BF",
-        "ring-start-bmo": "#EB6600",
-        "ring-end-bmo": "#FFCA7D",
-      },
-      backgroundImage: {
-        "radial-gradient":
-          "radial-gradient(circle,rgba(0,0,0,0.75) 25%,rgba(0,0,0,0) 100%)",
-      },
-      animation: {
-        'rotate': 'rotate 10s ease-in infinite both',
-      },
-    },
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/forms")({
-      strategy: "base",
-    }),
-    function ({ addUtilities }) {
-      const newUtilities = {};
-      // Define grid classes from 1 to 12 columns with a gap of 8
-      for (let i = 1; i <= 12; i++) {
-        newUtilities[`.grid-${i}`] = {
-          display: "flex",
-          flexDirection: "column",
-          gap: "2rem", // Corresponds to 'gap-8' in Tailwind's default spacing scale
-          "@screen lg": {
-            display: "grid",
-            gridTemplateColumns: `repeat(${i}, minmax(0, 1fr))`,
-            gap: "4rem", // Corresponds to 'gap-16' in Tailwind's default spacing scale
-          },
-        };
-      }
+	content: ['./**/*.php'],
+	darkMode: 'class',
+	theme: {
+		container: {
+			center: true,
+			padding: '1.5rem',
+			screens: {
+				sm: '768px',
+				md: '1024px',
+				lg: '1280px',
+				xl: '1536px',
+			},
+		},
+		fontSize: {
+			10: toRem(10),
+			11: toRem(11),
+			12: toRem(12),
+			13: toRem(13),
+			14: toRem(14),
+			15: toRem(15),
+			16: toRem(16),
+			17: toRem(17),
+			18: toRem(18),
+			20: toRem(20),
+			22: toRem(22),
+			24: toRem(24),
+			26: toRem(26),
+			28: toRem(28),
+			30: toRem(30),
+			32: toRem(32),
+			35: toRem(35),
+			40: toRem(40),
+			45: toRem(45),
+			50: toRem(50),
+			56: toRem(56),
+			60: toRem(60),
+			70: toRem(70),
+			80: toRem(80),
+			90: toRem(90),
+			120: toRem(120),
+		},
+		extend: {
+			fontFamily: {},
+			colors: {},
+			backgroundImage: {},
+			animation: {},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/forms')({
+			strategy: 'base',
+		}),
+		function ({ addUtilities }) {
+			const newUtilities = {};
+			// Define grid classes from 1 to 12 columns with a gap of 8
+			for (let i = 1; i <= 12; i++) {
+				newUtilities[`.grid-${i}`] = {
+					display: 'flex',
+					flexDirection: 'column',
+					gap: '2rem', // Corresponds to 'gap-8' in Tailwind's default spacing scale
+					'@screen lg': {
+						display: 'grid',
+						gridTemplateColumns: `repeat(${i}, minmax(0, 1fr))`,
+						gap: '4rem', // Corresponds to 'gap-16' in Tailwind's default spacing scale
+					},
+				};
+			}
 
-      addUtilities(newUtilities, ["responsive", "hover"]);
-    },
-    plugin(function ({ addVariant, e }) {
-      addVariant('bvg', ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-          return `.theme-bvg .${e(`bvg${separator}${className}`)}`;
-        });
-      });
-      addVariant('bmo', ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-          return `.theme-bmo .${e(`bmo${separator}${className}`)}`;
-        });
-      });
-    }),
-    plugin(({ addComponents, theme }) => {
-      addComponents({
-        '.gradient-ring': {
-          position: 'relative',
-          '&:before': {
-            content: '""',
-            position: 'absolute',
-            top: '-2px',
-            right: '-2px',
-            bottom: '-2px',
-            left: '-2px',
-            borderRadius: '100%',
-            zIndex: '-1',
-            animation: theme('animation.rotate'),
-          },
-        },
-        '.theme-bvg .gradient-ring': {
-          position: 'relative',
-          '&:before': {
-            background: `linear-gradient(90deg, ${theme('colors.ring-start-bvg')} 0%, ${theme('colors.ring-end-bvg')} 100%)`,
-          },
-        },
-        '.theme-bmo .gradient-ring': {
-          position: 'relative',
-          '&:before': {
-            background: `linear-gradient(90deg, ${theme('colors.ring-start-bmo')} 0%, ${theme('colors.ring-end-bmo')} 100%)`,
-          },
-        },
-      });
-    }),
-  ],
+			addUtilities(newUtilities, ['responsive', 'hover']);
+		},
+	],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,124 +1,124 @@
-const path = require("path");
-const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const LiveReloadPlugin = require("webpack-livereload-plugin");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = (_env, options) => {
-  const devMode = options.mode !== "production";
+	const devMode = options.mode !== 'production';
 
-  return {
-    watch: devMode,
-    watchOptions: {
-      aggregateTimeout: 200,
-      ignored: ["**/node_modules", "**/dist"],
-      poll: 1000,
-    },
-    entry: {
-      main: "./source/index.ts",
-    },
-    devtool: "source-map",
-    resolve: {
-      extensions: [".js", ".ts"],
-    },
-    output: {
-      filename: "[name].js",
-      path: path.resolve(__dirname, "dist/"),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(ts|js)x?$/,
-          use: [
-            {
-              loader: "thread-loader",
-              options: {
-                workers: 2,
-              },
-            },
-            {
-              loader: "babel-loader",
-            },
-          ],
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.css$/,
-          exclude: /node_modules\/(?!splitting)/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: "css-loader",
-              options: {
-                importLoaders: 1,
-              },
-            },
-            "postcss-loader",
-          ],
-        },
-        {
-          test: /\.(svg|png|jpg|ico|xml|webmanifest)$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "[name].[ext]",
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(otf|ttf|woff2?)$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "[contenthash].[ext]",
-                output: "fonts/",
-              },
-            },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new Dotenv({
-        systemvars: true,
-        path: path.resolve(__dirname, ".env"),
-      }),
-      new MiniCssExtractPlugin({
-        filename: "[name].css",
-        chunkFilename: "[id].css",
-      }),
-      new WebpackManifestPlugin({
-        generate: (_seed, files, _entries) => {
-          const manifest = {};
+	return {
+		watch: devMode,
+		watchOptions: {
+			aggregateTimeout: 200,
+			ignored: ['**/node_modules', '**/dist'],
+			poll: 1000,
+		},
+		entry: {
+			main: './source/index.ts',
+		},
+		devtool: 'source-map',
+		resolve: {
+			extensions: ['.js', '.ts'],
+		},
+		output: {
+			filename: '[name].js',
+			path: path.resolve(__dirname, 'dist/'),
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(ts|js)x?$/,
+					use: [
+						{
+							loader: 'thread-loader',
+							options: {
+								workers: 2,
+							},
+						},
+						{
+							loader: 'babel-loader',
+						},
+					],
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.css$/,
+					exclude: /node_modules\/(?!splitting)/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						{
+							loader: 'css-loader',
+							options: {
+								importLoaders: 1,
+							},
+						},
+						'postcss-loader',
+					],
+				},
+				{
+					test: /\.(svg|png|jpg|ico|xml|webmanifest)$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								name: '[name].[ext]',
+							},
+						},
+					],
+				},
+				{
+					test: /\.(otf|ttf|woff2?)$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								name: '[contenthash].[ext]',
+								output: 'fonts/',
+							},
+						},
+					],
+				},
+			],
+		},
+		plugins: [
+			new Dotenv({
+				systemvars: true,
+				path: path.resolve(__dirname, '.env'),
+			}),
+			new MiniCssExtractPlugin({
+				filename: '[name].css',
+				chunkFilename: '[id].css',
+			}),
+			new WebpackManifestPlugin({
+				generate: (_seed, files, _entries) => {
+					const manifest = {};
 
-          files.forEach((file) => {
-            if (file.chunk) {
-              let filename;
-              if (file.name.endsWith(".css")) {
-                filename = `${file.chunk.name}.css`;
-              } else {
-                filename = `${file.chunk.name}.js`;
-              }
-              manifest[file.name] = `${filename}?hash=${file.chunk.hash}`;
-            }
-          });
+					files.forEach(file => {
+						if (file.chunk) {
+							let filename;
+							if (file.name.endsWith('.css')) {
+								filename = `${file.chunk.name}.css`;
+							} else {
+								filename = `${file.chunk.name}.js`;
+							}
+							manifest[file.name] = `${filename}?hash=${file.chunk.hash}`;
+						}
+					});
 
-          return manifest;
-        },
-      }),
-      ...(devMode ? [new LiveReloadPlugin()] : []),
-    ],
-    cache: {
-      type: "filesystem",
-      cacheDirectory: path.resolve(__dirname, ".cache"),
-      buildDependencies: {
-        config: [__filename],
-      },
-    },
-  };
+					return manifest;
+				},
+			}),
+			...(devMode ? [new LiveReloadPlugin()] : []),
+		],
+		cache: {
+			type: 'filesystem',
+			cacheDirectory: path.resolve(__dirname, '.cache'),
+			buildDependencies: {
+				config: [__filename],
+			},
+		},
+	};
 };
